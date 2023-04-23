59f6294c89d179de5cf0486b4e5c92ec3fe41a23
anish2412 <anishsingh7777@gmail.com>
2023-04-23 16:27:59 +0530

    updating our app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/show.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' Component={HomePage} />
+        <Route path='/shop' Component={ShopPage} />
+      </Routes>
     </div>
   );
 }
